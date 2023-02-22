e46b68391f6f1655f426dbda7609045f36cca7cb
Meg2022 <megnkashe@gmail.com>
2023-02-22 01:08:57 -0500

    Modified terraform files

--- Compute/provider.tf
+++ Compute/provider.tf
@@ -10,15 +10,14 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA5L3T77ZFEMB7Q2PZ"
-  secret_key = "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"
+  
 }
 
 
 
 
 # Create a VPC
-resource "aws_vpc" "projectA" {
+resource "aws_vpc" "projectB" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "main"
